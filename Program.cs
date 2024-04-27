6d7766b08d38cc023d8cb8f100bc433b8a6c7cb8
Leifeng2000 <leifeng7112@gmail.com>
2024-04-27 21:38:54 +0700

    up

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Timers;
+
+internal class Program
+{
+    private static System.Timers.Timer aTimer;
+    public static void Main(string[] args)
+    {
+        aTimer = new System.Timers.Timer();
+        aTimer.Interval = 100;
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+        Console.WriteLine("nhan 1 phim bat ky ");
+        Console.ReadLine();
+    }
+    private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+    {
+        Console.WriteLine("Thong diep {0}", e.SignalTime);
+    }
+}
